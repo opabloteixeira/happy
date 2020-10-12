3760620ac6a4eb201cb7484922869f31ab77b5be
Pablo Teixeira <pablopst@gmail.com>
2020-10-12 20:44:15 -0300

    first day front end

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -33,12 +33,12 @@ const OrphanagesMap: React.FC = () => (
                   height: '100%'
                }}
             >
-               <TileLayer
-                  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-               />
                {/* <TileLayer
-                  url= {`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
                /> */}
+               <TileLayer
+                  url= {`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+               />
             </Map>
 
             <Link to="" className="create-orphanage">
